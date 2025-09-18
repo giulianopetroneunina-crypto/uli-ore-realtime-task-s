edf9fc29151aaf6e1a08d630880ccbefea1015ea
giulianopetroneunina-crypto <giuliano.petrone.unina@gmail.com>
2025-09-18 02:35:31 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "TUO_API_KEY",
+  authDomain: "TUA_APP.firebaseapp.com",
+  databaseURL: "https://TUA_APP.firebaseio.com",
+  projectId: "TUA_APP",
+  storageBucket: "TUA_APP.appspot.com",
+  messagingSenderId: "XXX",
+  appId: "APP_ID"
+};
+// Cambia con i tuoi dati da Firebase console!
